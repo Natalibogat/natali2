d5eae112e7d51183ec4d655a14263d69092915e5
Natalibogat <Natalibogat>
2022-02-18 21:24:03 +0300

    18/02JMenu

---
+++ src/RAAN.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RAAN {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Application");
+        frame.setSize(600, 400);
+
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JMenuBar menu = new JMenuBar();
+        JMenu menuHelloCli = new JMenu("HelloCli");
+        JMenuItem itemHelloEn=new JMenuItem("HelloEn");
+        menuHelloCli.add(itemHelloEn);
+        menu.add(menuHelloCli);
+        itemHelloEn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.out.println("Hello,world");
+            }
+        });
+
+
+        frame.setJMenuBar(menu);
+
+    }
+}
